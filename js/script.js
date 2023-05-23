71a27942366738578f3204e9302521a2128db41e
AdrianVacante <adrian.vacante@elev.ga.ntig.se>
2023-05-23 14:46:44 +0200

    Bildförminskning

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-const burger = document.getElementById('burger')
-const linkList = document.getElementById('link_list')
+ const burger = document.getElementById('burger')
+ const linkList = document.getElementById('link_list')
 
-function toggleMenu() {
-    linkList.classList.toggle('link_list_visible')
-    burger.classList.toggle('burger_in_x_form')
-}
+ function toggleMenu() {
+     linkList.classList.toggle('link_list_visible')
+     burger.classList.toggle('burger_in_x_form')
+ }
 
 
-burger.addEventListener('click', toggleMenu)
+ burger.addEventListener('click', toggleMenu)
+
+// const dropdownButton = document.getElementById("dropdown-btn");
+// const dropdownMenu = document.getElementById("dropdown-menu");
+
+// dropdownButton.addEventListener("click", function() {
+//   dropdownMenu.classList.toggle("show");
+// });
